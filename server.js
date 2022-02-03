bfe24f0a883b0e2da8fbe1c91e0a6cd174cbd39f
SafaeQ <safaedani009@gmail.com>
2022-02-03 12:11:18 +0100

    ok ..

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express()
 const port = 8000;
+const connect = require("./connection/db");
 
 const router = require('./router/routes')
 
 app.use('/', router)
 
-app.listen(port, () => {
-    console.log(`The server is listening on port http://localhost:${port}`);
-})
+connect()
+    .catch((error) => {
+        throw error
+    })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`The server is listening on port http://localhost:${port}`);
+        })
+    })
