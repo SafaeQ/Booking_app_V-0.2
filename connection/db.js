f823e5be8fcc47864130526bf66d6e607b903a21
SafaeQ <safaedani009@gmail.com>
2022-02-03 12:17:29 +0100

    conncet

--- connection/db.js
+++ connection/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-    await mongoose.connect('mongodb://localhost:27017/hotel-booking');
+    await mongoose.connect('mongodb://127.0.0.1:27017/hotel-booking');
 }
 
 module.exports = connect;
