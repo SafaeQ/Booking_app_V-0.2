91e90e2f78edcfe328e39c03e59fb85de0c9efb3
SafaeQ <safaedani009@gmail.com>
2022-02-03 12:08:00 +0100

    perfect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const port = 8000;
 
-const router = require('./routes/routes')
+const router = require('./router/routes')
 
 app.use('/', router)
 
