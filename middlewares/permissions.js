2d1d9c35680a1318f1dfe8265f0a3e6b78f6a5fb
SafaeQ <safaedani009@gmail.com>
2022-02-04 21:43:15 +0100

    👀add isAuthorized in file permissions

--- middlewares/permissions.js
+++ middlewares/permissions.js
@@ -1,27 +1,14 @@
-const jwt = require('express-jwt')
-const {
-    secret
-} = require('../config.json')
-
-const isAuthorized = (roles = []) => {
-    if (typeof roles === 'string') {
-        roles = [roles];
-    }
-    return [
-
-        jwt({
-            secret,
-            algorithms: ['HS256']
-        }),
-        (req, res, next) => {
-            if (roles.length && !roles.includes(req.user.role)) {
-                return res.status(401).json({
-                    message: 'Unauthorized'
-                });
-            }
+const isAuthorized = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.tokenData.role.name)) {
+            res.status(401).json({
+                error: "you are not authorized"
+            });
+        } else {
             next()
         }
-    ]
+    }
+
 }
 
 
