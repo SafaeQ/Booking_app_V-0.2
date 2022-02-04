80900b0d91ea5fa9f6b6804dfd1bae4d6655592d
SafaeQ <safaedani009@gmail.com>
2022-02-04 14:29:55 +0100

    dona

--- controllers/hotel.controller.js
+++ controllers/hotel.controller.js
@@ -15,8 +15,58 @@ const get_hotel_byId = async (req, res) => {
     res.status(200).send(hotel);
 }
 
+const add_hotel = async (req, res) => {
+    const {
+        name,
+        place,
+        stars
+    } = req.body;
+    const insertHotel = new Hotel({
+        name: name,
+        place: place,
+        stars: stars,
+        // images: images,
+    });
+    const hotel = await insertHotel.save();
+    if (!hotel) return res.status(400).send("Sorry Hotel Does Not Added!");
+    res.status(200).send("Hotel Added Successfully!");
+}
 
+const update_hotel = async (req, res) => {
+    const hotelId = req.params.id;
+    const hotel = await Hotel.findById(hotelId);
+    if (!hotel)
+        return res.status(400).send("Sorry We Can Not Find Hotel With Given Id!");
+    const {
+        name,
+        place,
+        stars
+    } = req.body;
+    const updateHotel = await Hotel.updateOne({
+        _id: hotelId
+    }, {
+        name: name,
+        place: place,
+        stars: stars
+    });
+    if (updateHotel) return res.status(200).send("Hotel Updated Successfully!");
+    res.send("Ooops Something Goes Wrong!");
+}
+
+const delete_hotel = async (req, res) => {
+    const hotelId = req.params.id;
+    const hotel = await Hotel.findById(hotelId);
+    if (!hotel)
+        return res.status(400).send("Sorry We Can Not Find Hotel With Given Id!");
+    await Hotel.deleteOne({
+        _id: hotelId
+    });
+    res.status(200).send("delete hotel");
+}
 module.exports = {
     get_hotels,
-    get_hotel_byId
+    get_hotel_byId,
+    add_hotel,
+    update_hotel,
+    delete_hotel
 }
--- router/hotel.routes.js
+++ router/hotel.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const hotelRouter = express.Router()
-
-hotelRouter.get('/hotels', )
-hotelRouter.get('/hotel/:id', )
-hotelRouter.post('/add-hotel', )
-hotelRouter.put('/hotel/:id/update', )
+const {
+    get_hotels,
+    get_hotel_byId,
+    add_hotel,
+    update_hotel,
+    delete_hotel
+} = require('../controllers/hotel.controller')
+hotelRouter.get('/hotels', get_hotels)
+hotelRouter.get('/hotel/:id', get_hotel_byId)
+hotelRouter.post('/add-hotel', add_hotel)
+hotelRouter.put('/hotel/:id/update', update_hotel)
 hotelRouter.delete('/hotel/:id/delete', )
 
 module.exports = hotelRouter
