6dd27979ec313433719f3edfa2ffbcfc71426124
SafaeQ <safaedani009@gmail.com>
2022-02-06 13:59:11 +0100

    😃set the structure of constroller

---
+++ controllers/room.controller.js
@@ -0,0 +1,35 @@
+const Room = require('../models/room')
+
+
+const getAll_rooms = async (req, res) => {
+
+}
+
+const getOne_room = async (req, res) => {
+
+}
+
+const add_room = async (req, res) => {
+
+}
+
+const update_room = async (req, res) => {
+
+}
+
+const removeOne_room = async (req, res) => {
+
+}
+
+const deleteAll_rooms = async (req, res) => {
+
+}
+
+module.exports = {
+    getAll_rooms,
+    getOne_room,
+    add_room,
+    update_room,
+    removeOne_room,
+    deleteAll_rooms
+}
