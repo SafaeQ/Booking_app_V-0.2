e09859cedfd799152391d97b62d1b2d0fbd3381e
SafaeQ <safaedani009@gmail.com>
2022-02-06 15:12:52 +0100

    ✔ update hotel

--- models/room.js
+++ models/room.js
@@ -5,26 +5,10 @@ const hotelSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-    },
-    place: {
-        type: String,
-        required: true,
-    },
-    stars: {
-        type: Number,
-        required: true,
-    },
-    images: {
-        type: Array,
-        required: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+    }
 });
 
-module.exports = mongoose.model("Hotel", hotelSchema);
+module.exports = mongoose.model("Hotels", hotelSchema);
 
 const roomSchema = new mongoose.Schema({
     type: {
