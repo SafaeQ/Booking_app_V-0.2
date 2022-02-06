0f52720175a0f99cbd6b6efe9e66bf7cc8cb58ab
SafaeQ <safaedani009@gmail.com>
2022-02-06 15:06:43 +0100

    set the routes

---
+++ router/room.routes.js
@@ -0,0 +1,18 @@
+const roomRouter = require('express').Router()
+const {
+    getAll_rooms,
+    getOne_room,
+    add_room,
+    update_room,
+    removeOne_room,
+    deleteAll_rooms
+} = require('../controllers/room.controller')
+roomRouter
+    .get('/rooms', getAll_rooms)
+    .get('/room/:id', getOne_room)
+    .post('/add-room', add_room)
+    .put('/room/:id/update', update_room)
+    .delete('/room/:id/delete', removeOne_room)
+    .delete('/rooms/delete', deleteAll_rooms)
+
+module.exports = roomRouter
