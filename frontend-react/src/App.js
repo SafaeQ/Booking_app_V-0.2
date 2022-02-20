0810fe83a93e751600396d67c4b04db421a34086
SafaeQ <safaedani009@gmail.com>
2022-02-20 19:25:15 +0100

    set auth

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Home} from './components/Home'
+import { Login} from './components/Login'
+import { Signup} from './components/Signup'
 import './App.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <h1>authentication</h1>
       <Home/>
+      <Login/>
+      <Signup/>
     </div>
   );
 }
