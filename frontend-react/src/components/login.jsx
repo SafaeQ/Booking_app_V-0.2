040ac5d9def25b27638974ee59b2d99f268f9c5e
SafaeQ <safaedani009@gmail.com>
2022-02-20 20:44:48 +0100

    done✔

--- frontend-react/src/components/login.jsx
+++ frontend-react/src/components/login.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image'
 
 export const Login = ()=> {
     return (
         <div>
-            <h1>Login</h1>
+            <section className="vh-100">
+                <div className="container h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100 ">
+                    <div className="col-lg-12 col-xl-11">
+                        <div className="card text-black" >
+                        <div className="card-body p-md-5">
+                            <div className="row justify-content-center">
+                            <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
+
+                                <form className="mx-1 mx-md-4">
+
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                    <div className="form-outline flex-fill mb-0">
+                                    <label className="form-label" >Your Email</label>
+                                    <input type="email"  className="form-control" />
+                                    </div>
+                                </div>
+
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                    <div className="form-outline flex-fill mb-0">
+                                    <label className="form-label" >Password</label>
+                                    <input type="password"  className="form-control" />
+                                    </div>
+                                </div>
+
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                    <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                                    <div className="form-outline flex-fill mb-0">
+                                    <label className="form-label" >Role</label>
+                                    <input type="text" className="form-control" />
+                                    </div>
+                                </div>
+
+                                <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                    <button type="button" className="btn btn-primary btn-lg">Submit</button>
+                                </div>
+
+                                </form>
+
+                            </div>
+                            <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                <Image src="https://as2.ftcdn.net/v2/jpg/02/34/32/13/1000_F_234321332_znN7GKnrrOV3V9Ol4DTQW5LL24zZQ1oF.jpg" className="img-fluid" alt="Sample image"/>
+
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
