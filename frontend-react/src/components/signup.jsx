5d64422a6e967e56a5df568ae9ced7fd21b29267
SafaeQ <safaedani009@gmail.com>
2022-02-21 09:41:59 +0100

    buttun clicked

--- frontend-react/src/components/signup.jsx
+++ frontend-react/src/components/signup.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import Image from 'react-bootstrap/Image'
+import {Image, Button} from 'react-bootstrap'
 // import {  } from "react-icons/bs";
 
 export const Signup = ()=> {
+
+    const hundleSubmit = (e)=> {
+        e.preventDefault();
+        console.log('i\'m clicked');
+    }
+
     return (
         <div>
             <section className="vh-100">
@@ -16,7 +22,7 @@ export const Signup = ()=> {
 
                                 <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                                <form className="mx-1 mx-md-4">
+                                <form onSubmit={hundleSubmit} className="mx-1 mx-md-4">
 
                                 <div className="d-flex flex-row align-items-center mb-4">
                                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
@@ -51,7 +57,7 @@ export const Signup = ()=> {
                                 </div>
 
                                 <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                    <button type="button" className="btn btn-primary btn-lg">Register</button>
+                                    <Button type="submit" className="btn btn-primary btn-lg">Register</Button>
                                 </div>
 
                                 </form>
