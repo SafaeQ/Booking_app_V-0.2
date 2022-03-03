8dd4804bb606058d340c2a27b1d7d5bcc3d61b9a
SafaeQ <safaedani009@gmail.com>
2022-03-03 13:58:42 +0100

    🐱‍👤 finish authservice _ login(axios)

--- frontend-react/src/services/authService.js
+++ frontend-react/src/services/authService.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 const URL = 'http://localhost:8000/auth'
 
 function login(email, password) {
-    
+    return axios.post(`${URL}/login`,{ 
+        email: email, password: password
+    })
 }
+
+module.exports = login
