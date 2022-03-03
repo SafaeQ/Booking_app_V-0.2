da1d574dde1f0848c452d529a67d7271bd7dc62f
SafaeQ <safaedani009@gmail.com>
2022-03-03 13:42:31 +0100

    ✔ done

--- frontend-react/src/components/signup.jsx
+++ frontend-react/src/components/signup.jsx
@@ -7,7 +7,6 @@ export const Signup = ()=> {
     const [user, setUser] = useState(initialForm)
 
     const handleInput = (event)=> {
-        // console.log(event);
         const {name, value} = event.target
         setUser(({...user, [name]: value}))
     }
